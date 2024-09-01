5a1729df329a1264b23b7698fa797001f4e7592a
Alpha-Abyss <Ramandeepsinghraman30@gmail.com>
2024-09-01 21:05:39 +0530

    Start developing player

--- SHADOW/main.py
+++ SHADOW/main.py
@@ -10,6 +10,25 @@ size = sw / 2, sh / 2
 Screen = pygame.display.set_mode(size, pygame.RESIZABLE)
 pygame.display.set_caption("Shadow")
 
+#----- Player
+class PLAYER(pygame.sprite.Sprite):
+    def __init__(self):
+        super().__init__()
+        self.sprit_type = {"idle": "Idle.png",
+                           "run": "Run.png",
+                           "jump": "Jump.png",
+                           "climb": "LedgeClimb64x64.png",
+                           "push": "Push.png",
+                           "wall-slid": "Wall-slid.png"}
+        self.load_sprite_sheet()
+        self.current_frame = 0
+        self.animation_frames = []
+        self.load_frames()
+    
+    def load_sprite_sheet(self):
+        self.image = pygame.
+        
+
 #----- MAIN
 def gameloop():
     running = True
